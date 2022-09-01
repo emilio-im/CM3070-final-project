2890e6f145e51bc9bb1785c1137f84473d161ddd
Emilio Villagran <emilio@villagran.cl>
2022-09-01 17:02:42 -0400

    feat(auth): add user credentials provider with mock account

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,3 +1,4 @@
+import CredentialsProvider from "next-auth/providers/credentials";
 import GitHubProvider from "next-auth/providers/github";
 import clientPromise from "@services/mongodb";
 import NextAuth from "next-auth";
@@ -22,13 +23,62 @@ export default NextAuth({
         };
       },
     }),
+    /**
+     * @notes
+     *  this is added just to mock a user data and to have all functionalities
+     *  without login in with a Github account. This user is a mock user and
+     *  credentials provider is not fully implemented.
+     */
+    CredentialsProvider({
+      id: "credentials",
+      name: "Credentials",
+      credentials: {
+        email: {
+          label: "Email",
+          type: "email",
+          placeholder: "jsmith@example.com",
+        },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials, req) {
+        const user = {
+          id: "999",
+          name: "J Smith",
+          email: "jsmith@example.com",
+          username: "jsmith@example.com",
+        };
+
+        if (user) return user;
+        else return null;
+      },
+    }),
   ],
+  /**
+   * @notes
+   *  callbacks are working in a compatible way to have the mock account (using
+   *  credentials provider) and using real account using GithubProvider.
+   */
   callbacks: {
-    async session({ session, user }) {
-      session.username = user.username;
-      session.user = { ...user };
+    async jwt(params) {
+      const session = params.token;
+      session.user = params.user;
 
       return session;
     },
+    async session({ session, user, token }) {
+      session.username = user?.username;
+      session.user = { ...(user || {}) } as any;
+
+      if (token) {
+        session.user = token as any;
+        session.user.id = token.sub!;
+        session.username = token.sub;
+      }
+
+      return session;
+    },
+  },
+  session: {
+    strategy: "jwt",
   },
 });
