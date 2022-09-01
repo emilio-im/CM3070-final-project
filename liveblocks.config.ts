82d7a75be44560873ddeb2c3d98d8332d4c459f9
Emilio Villagran <emilio@villagran.cl>
2022-08-31 20:41:41 -0400

    chore(real-time): use environment variable

--- liveblocks.config.ts
+++ liveblocks.config.ts
@@ -4,9 +4,7 @@ import { createRoomContext } from "@liveblocks/react";
 import type { BaseUserMeta } from "@liveblocks/client";
 
 const client = createClient({
-  publicApiKey:
-    process.env.NEXT_PUBLIC_LIVEBLOCKS_PUBLIC_KEY! ||
-    "pk_live_xg8p7s0qyryHwQ5pqmVls_TL",
+  publicApiKey: process.env.LIVEBLOCKS_PUBLIC_KEY!,
 });
 
 type Presence = {
@@ -17,10 +15,7 @@ type Presence = {
   message: string;
 };
 
-type Storage = {
-  // animals: LiveList<string>,
-  // ...
-};
+type Storage = {};
 
 type UserMeta = BaseUserMeta;
 
