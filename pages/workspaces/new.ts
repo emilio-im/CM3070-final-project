1cca2c2110ca50a2217d022c06db34ac3f21c0ab
Emilio Villagran <emilio@villagran.cl>
2022-09-01 22:55:56 -0400

    feat(workspaces): redirect to home if not logged in

--- pages/workspaces/new.ts
+++ pages/workspaces/new.ts
@@ -1,1 +1,14 @@
+import { getHomeRedirection } from "@utils/index";
+import { getSession } from "next-auth/react";
+
+import type { GetServerSideProps } from "next";
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const session = await getSession({ req: context.req });
+
+  if (!session) return getHomeRedirection();
+
+  return { props: {} };
+};
+
 export { default } from "@views/Teams/new";
