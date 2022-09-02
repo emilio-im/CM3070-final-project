62874f110c69a48a8b7fc6dca673bcbc4a512df8
Emilio Villagran <emilio@villagran.cl>
2022-09-02 11:27:33 -0400

    feat(landing): redirect to home if logged in

--- pages/guest.tsx
+++ pages/guest.tsx
@@ -1,3 +1,16 @@
 import Page from "@views/Editor";
 
+import { getHomeRedirection } from "@utils/index";
+import { getSession } from "next-auth/react";
+
+import type { GetServerSideProps } from "next";
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const session = await getSession({ req: context.req });
+
+  if (session) return getHomeRedirection();
+
+  return { props: {} };
+};
+
 export default Page;
