ddde2bf8189782fd0afcd5205a4e5a3ab6b1c989
Emilio Villagran <emilio@villagran.cl>
2022-09-01 21:19:24 -0400

    feat(profile): display documents

--- src/views/Profile/user-profile.tsx
+++ src/views/Profile/user-profile.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-
 import MainLayout from "@layout/main-layout";
 import Link from "next/link";
 
@@ -28,32 +27,44 @@ const Page: React.FC<Props> = ({ documents }) => {
 
   return (
     <MainLayout>
-      <div>
-        <h1 className="text-lg font-bold font-sans">Your documents</h1>
+      <div className="mb-4">
+        <h1 className="text-xl font-bold font-sans">Your documents</h1>
       </div>
 
-      {documentsGroupedByWorkspace.map((workspace) => (
-        <div key={`workspace-item-${workspace.id}`}>
-          <h2>{workspace.name}</h2>
+      {documentsGroupedByWorkspace?.length > 0 ? (
+        documentsGroupedByWorkspace.map((workspace) => (
+          <div key={`workspace-item-${workspace.id}`}>
+            <h2 className="font-sans font-bold text-lg">{workspace.name}</h2>
 
-          <ul className="my-2">
-            {workspace.documents.map((document) => (
-              <Link
-                key={document._id}
-                href={`/documents/${document._id}`}
-                passHref
-              >
-                <a>
-                  <li className="text-sm font-sans my-2 list-disc">
-                    {(document.blocks as Record<string, string>[])?.[0]?.html ||
-                      "New Document"}
-                  </li>
-                </a>
-              </Link>
-            ))}
-          </ul>
+            <ul className="my-2 px-4">
+              {workspace.documents.map((document) => (
+                <Link
+                  key={document._id}
+                  href={`/documents/${document._id}`}
+                  passHref
+                >
+                  <a>
+                    <li className="text-sm font-sans my-2 list-disc">
+                      {(document.blocks as Record<string, string>[])?.[0]
+                        ?.html || "New Document"}
+                    </li>
+                  </a>
+                </Link>
+              ))}
+            </ul>
+          </div>
+        ))
+      ) : (
+        <div>
+          <p className="font-sans text-sm">
+            You don&apos;t have documents. Start{" "}
+            {/* eslint-disable-next-line @next/next/no-html-link-for-pages */}
+            <a href="/documents/new" className="text-blue-600 font-sans">
+              creating one
+            </a>
+          </p>
         </div>
-      ))}
+      )}
     </MainLayout>
   );
 };
