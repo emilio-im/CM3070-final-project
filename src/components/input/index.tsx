71e3bb7866270847a51e0c7004cb5e2930f01c21
Emilio Villagran <emilio@villagran.cl>
2022-09-04 01:02:53 -0400

    feat(input): add styles to input

--- src/components/input/index.tsx
+++ src/components/input/index.tsx
@@ -1,12 +1,23 @@
 import React from "react";
 
-type Props = React.DetailedHTMLProps<
+type Props = { label?: string } & React.DetailedHTMLProps<
   React.InputHTMLAttributes<HTMLInputElement>,
   HTMLInputElement
 >;
 
-const Input: React.FC<Props> = ({ ...props }) => (
-  <input {...props} className="hover:bg-red-900" />
+const Input: React.FC<Props> = ({ label, ...props }) => (
+  <div className="flex flex-col">
+    {label ? (
+      <label htmlFor={props.id} className="text-sm text-gray-500 mb-1 pl-4">
+        {label}
+      </label>
+    ) : null}
+
+    <input
+      {...props}
+      className="font-sans rounded-lg px-4 py-2 focus:bg-gray-100 outline-none hover:bg-gray-50"
+    />
+  </div>
 );
 
 export default Input;
